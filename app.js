4cf18e7416fff32069bd6cc48cc82c5d38c6fb9e
Akeon <danakhdan12@gmail.com>
2024-03-28 13:02:12 +0700

    feat : test and binding routes to app

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const app = express()
 const port = 3000
-
+const routes = require("./routes/v1/index")
 
 app.use(express.json())
 
+app.use("/api/v1", routes)
 
 app.listen(port, () => {
     console.log("server run in port", port)
